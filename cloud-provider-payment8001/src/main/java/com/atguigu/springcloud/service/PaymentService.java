3487f4a5900e749419519fcafc94ab753d41bfc2
jiuchun <862944586@qq.com>
2021-12-26 23:25:12 +0800

    hot-fixcommit

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -9,5 +9,9 @@ import org.apache.ibatis.annotations.Param;
  */
 public interface PaymentService {
     public int create(Payment payment);
+
+
+
+
     public Payment getPaymentById(@Param("id") Long id);
 }
